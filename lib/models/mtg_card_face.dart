ff2cbbbcffa0cce1f3c5f6e2ebe2bb55df5188b8
zmuranaka <zmuranaka@gmail.com>
2023-10-30 10:12:36 -0600

    Create MTGCardFace model

---
+++ lib/models/mtg_card_face.dart
@@ -0,0 +1,105 @@
+import 'package:equatable/equatable.dart';
+import 'package:flutter/material.dart';
+import 'package:magic_the_gathering_flutter/errors_and_exceptions.dart';
+import 'package:magic_the_gathering_flutter/models/mtg_symbology.dart';
+
+class MTGCardFace extends Equatable {
+  final String? artist;
+  final String? flavorText;
+  final Map<String, String>? images;
+  final String? manaCost;
+  final double? manaValue;
+  final String name;
+  final String? oracleText;
+  final String? power;
+  final String? toughness;
+  final String typeLine;
+
+  /// [manaValue] not included because [manaCost] serves the same purpose.
+  @override
+  List<Object?> get props => [
+        artist,
+        flavorText,
+        images,
+        manaCost,
+        name,
+        oracleText,
+        power,
+        toughness,
+        typeLine,
+      ];
+
+  MTGCardFace.fromMap(Map m)
+      : artist = m['artist'],
+        flavorText = m['flavor_text'],
+        images = _images(m['image_uris']),
+        manaCost = m['mana_cost'],
+        manaValue = m['cmc'],
+        name = m['name'],
+        oracleText = m['oracle_text'],
+        power = m['power'],
+        toughness = m['toughness'],
+        typeLine = m['type_line'];
+
+  static Map<String, String>? _images(dynamic value) {
+    if (value == null) {
+      return null;
+    }
+    if (value is! Map) {
+      throw BadDataError('images $value was unexpected');
+    }
+    if (value.isEmpty) {
+      return null;
+    }
+    return value.cast<String, String>();
+  }
+
+  List<Padding>? preparedManaCost() {
+    if (manaCost == null) {
+      return null;
+    }
+    final matches = MTGSymbol.regex.allMatches(manaCost!);
+    if (matches.isEmpty) {
+      return null;
+    }
+    final List<Padding> manaCostSymbols = [];
+    for (final match in matches) {
+      final MTGSymbol? mtgSymbol = mtgSymbology[match.group(0)];
+      if (mtgSymbol == null) {
+        throw BadDataError('symbol ${match.group(0)} was unexpected');
+      }
+      manaCostSymbols.add(
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 1.5),
+          child: mtgSymbol.toSvg(),
+        ),
+      );
+    }
+    return manaCostSymbols;
+  }
+
+  TextSpan? preparedOracleText() {
+    if (oracleText == null) {
+      return null;
+    }
+    final matches = MTGSymbol.regex.allMatches(oracleText!);
+    if (matches.isEmpty) {
+      return TextSpan(text: oracleText);
+    }
+    final List<InlineSpan> children = [];
+    int lastIndex = 0;
+    for (final match in matches) {
+      children.add(
+        TextSpan(text: oracleText!.substring(lastIndex, match.start)),
+      );
+      final MTGSymbol? mtgSymbol = mtgSymbology[match.group(0)];
+      if (mtgSymbol == null) {
+        throw BadDataError('symbol ${match.group(0)} was unexpected');
+      }
+      children.add(WidgetSpan(child: mtgSymbol.toSvg()));
+      lastIndex = match.end;
+    }
+    children.add(TextSpan(text: oracleText!.substring(lastIndex)));
+    return TextSpan(children: children);
+  }
+}
