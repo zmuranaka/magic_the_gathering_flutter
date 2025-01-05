4e13877911e8726afb45efdd2b5a2ce165a27227
zmuranaka <zmuranaka@gmail.com>
2025-01-04 20:43:05 -0700

    Add rarity & releasedAt to MTGCard.props * One could reasonably expect that if the set is expected to change equality that the rarity & releasedAt should as well * Also add dartdoc comments * Also move a few of the things around

--- lib/models/mtg_card.dart
+++ lib/models/mtg_card.dart
@@ -1,6 +1,9 @@
 import 'package:magic_the_gathering_flutter/enums/rarity.dart';
 import 'package:magic_the_gathering_flutter/models/mtg_card_face.dart';
 
+/// Represents a full Magic: The Gathering card.
+/// If the card has multiple faces, each of its faces is represented by an
+/// [MTGCardFace] instance in [cardFaces].
 class MTGCard extends MTGCardFace {
   final List<MTGCardFace>? cardFaces;
   final List<String> keywords;
@@ -11,33 +14,9 @@ class MTGCard extends MTGCardFace {
   final String setCode;
   final String setName;
 
-  /// [rarity] and [releasedAt] not included because they are subject to change
-  /// without the actual card changing.
-  ///
-  /// [setName] not included because [setCode] serves the same purpose.
-  @override
-  List<Object?> get props =>
-      [
-        cardFaces,
-        keywords,
-        language,
-        setCode,
-        reserved,
-      ] +
-      super.props;
-
-  bool get hasMultipleFaces => cardFaces?.isNotEmpty == true;
-
-  /// Ratio of card height / width : 3.5 inches / 2.5 inches
-  ///
-  /// See https://draftsim.com/mtg-card-size/
-  static const sizeRatio = 1.4;
-
-  /// Ratio of card height / corner radius: 3.5 inches / (1/8 inch)
-  ///
-  /// See https://draftsim.com/mtg-card-size/
-  static const cornerRatio = 28;
-
+  /// Convert a map to an [MTGCard] instance.
+  /// The intended use case is to store or request JSON data and convert that
+  /// to a Dart [Map], then pass that to this constructor.
   MTGCard.fromMap(Map m)
       : cardFaces = _cardFaces(m['card_faces']),
         keywords = ((m['keywords'] ?? []) as List).cast<String>(),
@@ -49,6 +28,7 @@ class MTGCard extends MTGCardFace {
         setName = m['set_name'],
         super.fromMap(m);
 
+  /// Method used to parse [cardFaces]. Not intended for external use.
   static List<MTGCardFace>? _cardFaces(dynamic value) {
     if (value == null) {
       return null;
@@ -61,4 +41,32 @@ class MTGCard extends MTGCardFace {
     }
     return [for (final cardFace in value) MTGCardFace.fromMap(cardFace)];
   }
+
+  /// Returns true if the card has multiple faces
+  bool get hasMultipleFaces => cardFaces?.isNotEmpty == true;
+
+  /// Ratio of card height / width : 3.5 inches / 2.5 inches
+  ///
+  /// See https://draftsim.com/mtg-card-size/
+  static const sizeRatio = 1.4;
+
+  /// Ratio of card height / corner radius: 3.5 inches / (1/8 inch)
+  ///
+  /// See https://draftsim.com/mtg-card-size/
+  static const cornerRatio = 28;
+
+  /// Extension of [MTGCardFace.props].
+  /// [setName] not included because [setCode] serves the same purpose.
+  @override
+  List<Object?> get props =>
+      [
+        cardFaces,
+        keywords,
+        language,
+        rarity,
+        releasedAt,
+        reserved,
+        setCode,
+      ] +
+      super.props;
 }
