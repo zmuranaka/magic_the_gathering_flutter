581b475c131e41b7222ee39a823ceb9c99e0e848
zmuranaka <zmuranaka@gmail.com>
2025-01-04 20:40:17 -0700

    Add Rarity dartdoc comments

--- lib/enums/rarity.dart
+++ lib/enums/rarity.dart
@@ -1,3 +1,4 @@
+/// Represents a Magic: The Gathering card rarity.
 enum Rarity {
   common,
   uncommon,
@@ -6,6 +7,8 @@ enum Rarity {
   special,
   bonus;
 
+  /// Convert a [String] to a [Rarity]. If the [String] does not match a valid
+  /// [Rarity], an [ArgumentError] is thrown.
   static Rarity fromString(String rarity) {
     return switch (rarity.toLowerCase()) {
       'common' => common,
