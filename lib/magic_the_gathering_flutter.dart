ee6cb81d0432fb696d7461703a137ce541af2a47
Brian Neumann-Fopiano <mypsn1550@gmail.com>
2025-03-28 23:18:06 -0400

    exports

--- lib/magic_the_gathering_flutter.dart
+++ lib/magic_the_gathering_flutter.dart
@@ -1,1 +1,10 @@
 library;
+
+// Individual component exports for direct access
+export 'enums/rarity.dart';
+export 'models/mtg_card.dart';
+export 'models/mtg_card_face.dart';
+export 'models/mtg_symbology.dart';
+
+// Convenience exports
+export 'models/mtg_symbology.dart' show MTGSymbol, mtgSymbology;
