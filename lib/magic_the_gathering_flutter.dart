e0c0232e7642fc82a3a3cead4474135e816c0f41
Brian Neumann-Fopiano <mypsn1550@gmail.com>
2025-03-28 23:55:36 -0400

    corrected

--- lib/magic_the_gathering_flutter.dart
+++ lib/magic_the_gathering_flutter.dart
@@ -6,7 +6,6 @@ export 'enums/rarity.dart';
 // Models - full exports for all classes and their methods
 export 'models/mtg_card.dart';
 export 'models/mtg_card_face.dart';
-export 'models/mtg_symbology.dart';
 
 // Utility functions for direct string handling
-export 'utils/mtg_symbol_utils.dart';
+export 'package:magic_the_gathering_flutter/models/mtg_symbology.dart';
