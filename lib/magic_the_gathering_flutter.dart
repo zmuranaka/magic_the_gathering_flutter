c205d797d1d28882ccd2e41645c04c8e4c1ba2f0
zmuranaka <zmuranaka@gmail.com>
2025-04-03 21:04:09 -0600

    Add exports to magic_the_gathering_flutter.dart

--- lib/magic_the_gathering_flutter.dart
+++ lib/magic_the_gathering_flutter.dart
@@ -1,1 +1,11 @@
+/// A library for helping render Magic: The Gathering cards and symbols as
+/// Flutter widgets.
+///
+/// See [here](https://pub.dev/packages/magic_the_gathering_flutter/example)
+/// for a full example.
 library;
+
+export 'enums/rarity.dart';
+export 'models/mtg_card.dart';
+export 'models/mtg_card_face.dart';
+export 'models/mtg_symbology.dart';
